e547b4f6215ae67ae2536c44e8064325794a5229
Cédric Uden <cedric@uden.de>
2021-06-15 14:00:02 +0200

    refactored main and rewrote DEFINE toggles to quickly test different scenarios

--- cuda_examples/matrix_multiply.cu
+++ cuda_examples/matrix_multiply.cu
@@ -4,8 +4,15 @@
 #include <string.h>
 #include "Runtime_Analysis.h"
 
+#define RUN_GPU true
+#define RUN_CPU true
+#define VERIFY_ARRAYS_MATCH true
+
+
+#define N 1024        // Dimension: width and height of matrix
+
+
 #define TARGET_INDEX (row * N + col)
-#define N 64        // Dimension: width and height of matrix
 
 __global__ void matrixMultiplyGPU(int *a, int *b, int *result) {
 
@@ -31,89 +38,95 @@ void matrixMultiplyCPU(int *a, int *b, int *result) {
     }
 }
 
+void initMatrix(int *a, int *b, int *result) {
+    for (int row = 0; row < N; ++row) {
+        for (int col = 0; col < N; ++col) {
+            a[TARGET_INDEX] = row;
+            b[TARGET_INDEX] = col + 2;
+            result[TARGET_INDEX] = 0;
+        }
+    }
+}
+
 int main() {
     int *a_cpu, *b_cpu, *result_cpu;
     int *a_gpu, *b_gpu, *result_gpu;
     int size = N * N * sizeof(int);
 
-    // Allocate memory on CPU
-    a_cpu = (int *) malloc(size);
-    b_cpu = (int *) malloc(size);
-    result_cpu = (int *) malloc(size);
+    if (RUN_GPU) {
+        // Allocate memory on GPU
+        cudaMallocManaged(&a_gpu, size);
+        cudaMallocManaged(&b_gpu, size);
+        cudaMallocManaged(&result_gpu, size);
+        initMatrix(a_gpu, b_gpu, result_gpu);
 
-    // Allocate memory on GPU
-    cudaMallocManaged(&a_gpu, size);
-    cudaMallocManaged(&b_gpu, size);
-    cudaMallocManaged(&result_gpu, size);
+        dim3 threads_per_block(16, 16, 1);    // 16 x 16 Block-Threads
+        dim3 number_of_blocks((N / threads_per_block.x) + 1,
+                              (N / threads_per_block.y) + 1,
+                              1);    // Two-Dimensional Grid: z = 1
+        auto *timer_gpu = new Runtime_Analysis("TimerGPU");
+        timer_gpu->setStart();
+        matrixMultiplyGPU <<<number_of_blocks, threads_per_block>>>(a_gpu, b_gpu, result_gpu);
+        timer_gpu->setEnd();
+        std::cout << timer_gpu->print(TimerUnits::microseconds).rdbuf();
 
-    // Initialize Matrices
-    for (int row = 0; row < N; ++row) {
-        for (int col = 0; col < N; ++col) {
-            a_cpu[TARGET_INDEX] = row;
-            b_cpu[TARGET_INDEX] = col + 2;
-            result_cpu[TARGET_INDEX] = 0;
-            a_gpu[TARGET_INDEX] = row;
-            b_gpu[TARGET_INDEX] = col + 2;
-            result_gpu[TARGET_INDEX] = 0;
-        }
+        cudaDeviceSynchronize();
     }
 
 
-    dim3 threads_per_block(16, 16, 1);    // 16 x 16 Block-Threads
-    dim3 number_of_blocks((N / threads_per_block.x) + 1,
-                          (N / threads_per_block.y) + 1,
-                          1);    // Two-Dimensional Grid: z = 1
+    if (RUN_CPU) {
+        // Allocate memory on CPU
+        a_cpu = (int *) malloc(size);
+        b_cpu = (int *) malloc(size);
+        result_cpu = (int *) malloc(size);
+        initMatrix(a_cpu, b_cpu, result_cpu);
 
-    auto *timer_gpu = new Runtime_Analysis("TimerGPU");
-    timer_gpu->setStart();
-    matrixMultiplyGPU <<<number_of_blocks, threads_per_block>>>(a_gpu, b_gpu,
-                                                                result_gpu);
-    timer_gpu->setEnd();
-    std::cout << timer_gpu->print(TimerUnits::microseconds).rdbuf();
-
-    cudaDeviceSynchronize();
-
-//    printf("\n");
-
-    auto *timer_cpu = new Runtime_Analysis("TimerCPU");
-    timer_cpu->setStart();
-    matrixMultiplyCPU(a_cpu, b_cpu, result_cpu);
-    timer_cpu->setEnd();
-    std::cout << timer_cpu->print(TimerUnits::microseconds).rdbuf();
-
-    bool error = false;
+        // run matrix multiplication on cpu
+        auto *timer_cpu = new Runtime_Analysis("TimerCPU");
+        timer_cpu->setStart();
+        matrixMultiplyCPU(a_cpu, b_cpu, result_cpu);
+        timer_cpu->setEnd();
+        std::cout << timer_cpu->print(TimerUnits::microseconds).rdbuf();
+    }
 
 
-    auto *timer_comparison = new Runtime_Analysis("Timer compare both arrays");
-    timer_comparison->setStart();
-    for (int row = 0; row < N && !error; ++row) {
+    if (VERIFY_ARRAYS_MATCH) {
+        //    printf("\n");
+        bool error = false;
 
-        for (int col = 0; col < N && !error; ++col) {
-
-//            printf("Values on \t\tGPU: %d\t\tCPU: %d\t\t\t", result_gpu[TARGET_INDEX], result_cpu[TARGET_INDEX]);
-//            printf("at index %d\n", TARGET_INDEX);
-
-            char* error_message = "Error in matrix multiplication at position ";
-            if (result_gpu[TARGET_INDEX] != result_cpu[TARGET_INDEX]) {
-                printf("%s[%d][%d]\n", error_message, row, col);
-                error = true;
-                break;
+        auto *timer_comparison = new Runtime_Analysis("Timer compare both arrays");
+        timer_comparison->setStart();
+        for (int row = 0; row < N && !error; ++row) {
+            for (int col = 0; col < N && !error; ++col) {
+    //            printf("Values on \t\tGPU: %d\t\tCPU: %d\t\t\t", result_gpu[TARGET_INDEX], result_cpu[TARGET_INDEX]);
+    //            printf("at index %d\n", TARGET_INDEX);
+                char* error_message = "Error in matrix multiplication at position ";
+                if (result_gpu[TARGET_INDEX] != result_cpu[TARGET_INDEX]) {
+                    printf("%s[%d][%d]\n", error_message, row, col);
+                    error = true;
+                    break;
+                }
             }
         }
-    }
-    timer_comparison->setEnd();
-    std::cout << timer_comparison->print(TimerUnits::microseconds).rdbuf();
-    if (!error) {
-        printf("Success!\n");
+        timer_comparison->setEnd();
+        if (!error) {
+            printf("Success! ");
+        }
+        std::cout << timer_comparison->print(TimerUnits::microseconds).rdbuf();
     }
 
-    cudaFree(a_gpu);
-    cudaFree(b_gpu);
-    cudaFree(result_gpu);
 
-    free(a_cpu);
-    free(b_cpu);
-    free(result_cpu);
+    if (RUN_GPU) {
+        cudaFree(a_gpu);
+        cudaFree(b_gpu);
+        cudaFree(result_gpu);
+    }
+
+    if (RUN_CPU) {
+        free(a_cpu);
+        free(b_cpu);
+        free(result_cpu);
+    }
 
     return 0;
 }
