6e449f64d100cb65419ba7faf1d645d4781af948
Cédric Uden <cedric@uden.de>
2021-06-15 07:37:58 +0200

    implemented the timer in cuda code

--- hello_world/hello_world_in_cuda.cu
+++ hello_world/hello_world_in_cuda.cu
@@ -1,6 +1,10 @@
 // sourcecode copy/pasta from https://gpgpu.io/2019/12/07/cuda-hello-world/
 
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+#include "Runtime_Analysis.h"
 
 // CUDA hello world kernel
 __global__ void hello_world() {
@@ -12,14 +16,21 @@ __global__ void hello_world() {
 int main(int argc, char *argv[]) {
     int exit = 0;
 
+    auto *timer = new Runtime_Analysis("Timer");
+
+    timer->setStart();
     // GPU hello world
     hello_world<<<1,1>>>();
+    timer->setEnd();
 
     // This is not an explicit "flush buffer" function, but it serves that purpose here.
     // Without this call it is very likely there won't be output from the GPU.
     cudaDeviceSynchronize();
     // basic CUDA error checking
     cudaError_t err = cudaGetLastError();
+
+    std::cout << timer->print(TimerUnits::milliseconds).rdbuf();
+
 
     if (err != cudaSuccess) {
         std::cout << "Error: " << cudaGetErrorString(err) << std::endl;
